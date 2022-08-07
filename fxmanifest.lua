cae92ea6fc23633e51ca8243f27c9f6ac785df10
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2022-08-06 21:16:58 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Constant Development'
 description 'QBCore Grandma Medical System'
-version '1.0.0'
+version '1.5'
 
 shared_script 'config.lua'
 client_script 'client/main.lua'
